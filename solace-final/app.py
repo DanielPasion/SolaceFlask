fa0fb89a54cb51dd6aaae456b81e13f64d53df1c
Daniel Pasion <dnpaxion@gmail.com>
2023-06-18 04:15:04 -0700

    fully functioning openai

--- solace-final/app.py
+++ solace-final/app.py
@@ -11,6 +11,25 @@ from pprint import pprint
 from hume import HumeStreamClient
 import asyncio
 import cv2
+            
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        prompt = request.form["prompt"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(prompt),
+            temperature=.6,
+            max_tokens=1000
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
 
 def capture_image():
     # Create a VideoCapture object
@@ -44,30 +63,12 @@ def findEmotion (result):
             max = result[i]['score']
             emotion = result[i]['name']
     return [emotion,max]
-            
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        emotion = request.form["emotion"]
-        prompt = request.form["prompt"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(prompt),
-            temperature=.6,
-            max_tokens=100
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
 
 def generate_prompt(prompt):
-    while True:
+    
+    while (True):
+        global emotion
+        emotion = ""
         capture_image()
 
         async def main():
@@ -78,11 +79,15 @@ def generate_prompt(prompt):
                 print(result['face'])
                 try:
                     result['face']['predictions']
-                    print(findEmotion(result['face']['predictions'][0]['emotions']))
+                    global emotion
+                    emotion = findEmotion(result['face']['predictions'][0]['emotions'])
+                    print(emotion[0])
                 except:
                     print("Faceless")
-
-
-            asyncio.run(main())
+            
+        asyncio.run(main())
     
-    return "Write a response to this following prompt: " + prompt + "if the person was feeling very " + emotion
+        return """Your name is Solace and you are an Artifical Intelligence therapist. You are given the task to find the best, most comforting ways to respond to people's issues, concerns, or anything they want to talk about.
+        You are most known for your superpower of being able to tell how a person is feeling just by the look of their face. Knowing this, a person just
+        spoke to you about """ + prompt + ", give a reponse to them knowing they feel " + emotion[0] + "but dont mention their emotion. Also, you will always remember everything somebody says for a session and will bring it up if mentioned before."
+        
